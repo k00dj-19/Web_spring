e73c6f7e2e63684f7012909f8ed7f9627d687f67
김동진 <rlaehdwls310@khu.ac.kr>
2021-10-20 11:17:15 +0000

    http 요청 실습2

--- src/main/java/project/test/HttpControllerTest.java
+++ src/main/java/project/test/HttpControllerTest.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
+import org.springframework.web.bind.annotation.RequestParam;
+import project.test.Member;
 // 사용자가 요청 -> 응답(HTML 파일)
 // @Controller
 
@@ -14,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
   
+  // 인터넷 브라우저 요청은 무조건 get 요청밖에 할 수 없다.
   //https://web-spring-ysnkx.run.goorm.io/http/get (select)
   @GetMapping("/http/get")
-  public String getTest() {
-    return "get 요청";
+  //public String getTest(@RequestParam int id, @RequestParam String username) {  // RequestParam을 사용하여 하나씩 받는 방법
+  public String getTest(Member m) {  // 생성자를 사용하여 한꺼번에 받는 방법
+    return "get 요청: " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
   }
   
   //https://web-spring-ysnkx.run.goorm.io/http/post (insert)
