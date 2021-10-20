9b3cc91dfc545c406db2d90d27185e5596eeeb23
김동진 <rlaehdwls310@khu.ac.kr>
2021-10-20 14:52:36 +0000

    lombok 세팅 완료

--- src/main/java/project/test/HttpControllerTest.java
+++ src/main/java/project/test/HttpControllerTest.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 //import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import project.test.Member;
 // 사용자가 요청 -> 응답(HTML 파일)
 // @Controller
 
@@ -15,7 +15,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 public class HttpControllerTest {
   
-  // 인터넷 브라우저 요청은 무조건 get 요청밖에 할 수 없다.
+  private static final String TAG = "HttpControllerTest : ";
+  
+  @GetMapping("/http/lombok")
+  public String lombokTest() {
+    Member m = new Member(1, "kdj", "1234", "kdj@naver.com");
+    System.out.println(TAG+"getter : "+m.getId());
+    m.setId(5000);
+    System.out.println(TAG+"getter : "+m.getId());
+    return "lombok test 완료";
+  }
+  
+  
+  
+  // 인터넷 브라우저 요청은 무조건 get 요청밖에 할 수 없다. 따라서 postman사용해서 put,post,delete해보기
   //https://web-spring-ysnkx.run.goorm.io/http/get (select)
   @GetMapping("/http/get")
   //public String getTest(@RequestParam int id, @RequestParam String username) {  // RequestParam을 사용하여 하나씩 받는 방법
@@ -47,7 +60,7 @@ public class HttpControllerTest {
   //https://web-spring-ysnkx.run.goorm.io/http/put (update)
   @PutMapping("/http/put")
   public String putTest(@RequestBody Member m) {
-    return "put 요청" + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
+    return "put 요청: " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
   }
   
   //https://web-spring-ysnkx.run.goorm.io/http/delete (delete)
--- src/main/java/project/test/Member.java
+++ src/main/java/project/test/Member.java
@@ -1,11 +1,25 @@
 package project.test;
 
+import lombok.Data;
+//import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+//@Getter
+//@Setter
+@Data  // Data = Getter + Setter
+@AllArgsConstructor  // 모든 필드를 사용하는 생성자 생성, 요즘엔 RequiredArgsConstructor을 많이 사용.
+//@RequiredArgsConstructor  // 변수 앞에 final이 붙은 필드에 대한 생성자 생성
+@NoArgsConstructor  // 빈 생성자
 public class Member {
   private int id;
   private String username;
   private String password;
   private String email; 
   
+  /*
   public Member(int id, String username, String password, String email) {
     this.id = id;
     this.username = username;
@@ -37,5 +51,6 @@ public class Member {
   public void setEmail(String email) {
     this.email = email;
   }
+  */
 }
 
