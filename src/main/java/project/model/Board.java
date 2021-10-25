799bc992bde9a3ed87425678121c348cb633f707
김동진 <rlaehdwls310@khu.ac.kr>
2021-10-25 11:09:43 +0000

    Board, User, Reply 연관관계의 주인

--- src/main/java/project/model/Board.java
+++ src/main/java/project/model/Board.java
@@ -1,6 +1,7 @@
 package project.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.FetchType;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -39,10 +42,13 @@ public class Board {
   @ColumnDefault("0")
   private int count;  // 조회수
   
-  @ManyToOne   // 연관관계. Many = Board, User = One. 한명의 User가 많은 Board 작성 가능.
+  @ManyToOne(fetch = FetchType.EAGER)   // 연관관계. Many = Board, User = One. 한명의 User가 많은 Board 작성 가능. 기본 fatch 전략이 EAGER. select하면 바로바로 가져옴.
   @JoinColumn(name="userId")  // 필드 값이 userId로 만들어짐.
   private User user;  // 자바는 오브젝트를 저장할 수 있지만, DB는 오브젝트를 저장할 수 없다. 따라서 foreign key를 사용.
   
+  @OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy가 적혀있으면, 연관관계의 주인이 아니라는 뜻. -> 난 FK가 아니에요. DB에 column을 만들지 마세요.
+  // One = Board, Many = reply. 기본 fatch 전략이 LAZY. 개수가 많으니까 필요하면 들고오고, 안필요하면 안들고 올게. 그러나 여기선 게시판 상세보기하면 댓글을 바로 가져와야 하니까 fatch전략을 EAGER로 변경.
+  private List<Reply> reply;  // Board를 select할 때, join문을 통해 값을 얻기 위한 것.
   
   @CreationTimestamp
   private Timestamp createDate;
