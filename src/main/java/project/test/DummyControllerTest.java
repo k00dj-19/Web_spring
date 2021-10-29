3a193575594ab170e9e987290605ee656bb95359
김동진 <rlaehdwls310@khu.ac.kr>
2021-10-29 14:40:50 +0000

    update 테스트

--- src/main/java/project/test/DummyControllerTest.java
+++ src/main/java/project/test/DummyControllerTest.java
@@ -3,12 +3,15 @@ package project.test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
 
 import project.model.User;
 import project.repository.UserRepository;
@@ -23,6 +26,31 @@ public class DummyControllerTest {
   @Autowired  // 의존성 주입(DI)
   // @Autowired 안쓰면 DummyControllerTest를 @RestController가 메모리에 로드할 때 userRepository가 null이다. 써주면 같이 메모리에 로드해주어 사용하기만 하면됨.
   private UserRepository userRepository;
+  
+  
+  // save 함수는 id를 전달하지 않으면 insert를 해주고,
+  // id를 전달할 때, 해당 id에 대한 데이터가 없으면 insert.
+  // id를 전달할 때, 해당 id에 대한 데이터가 있으면 update해줌.
+  // email, password만 수정 가능
+  @Transactional  // 다음 강의에서 설명. save를 하지 않아도 update가 된다!! => 더티체킹.
+  @PutMapping("/dummy/user/{id}")
+  public User updateUser(@PathVariable int id, @RequestBody User requestUser){  // Json 데이터를 요청 -> 스프링의 MessageConverter의 Jackson라이브러리가 Java Object로 변환해서 받아줌.
+    System.out.println("id: "+id);
+    System.out.println("password: "+requestUser.getPassword());
+    System.out.println("email: "+requestUser.getEmail());
+    
+    User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+      @Override
+      public IllegalArgumentException get() {
+        return new IllegalArgumentException("수정에 실패하였습니다.");
+      }
+    });
+    user.setPassword(requestUser.getPassword());
+    user.setEmail(requestUser.getEmail());
+    
+    // userRepository.save(user);
+    return null;
+  }
   
   // https://web-spring-ysnkx.run.goorm.io/blog/dummy/user
   @GetMapping("/dummy/users")
