61380d5bc83d904840f59d5b24038838ca80be17
김동진 <rlaehdwls310@khu.ac.kr>
2021-11-12 10:32:12 +0000

    ResponseDto 수정

--- src/main/java/project/handler/GlobalExceptionHandler.java
+++ src/main/java/project/handler/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package project.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import project.dto.ResponseDto;
 
 @ControllerAdvice  // 모든 exception이 이쪽으로 오게끔 설정.
 @RestController
 public class GlobalExceptionHandler {
   
   @ExceptionHandler(value=Exception.class)  // 모든 Exception이 발생하면 그 에러에 대한 함수를 e에 전달.
-  public String handleArgumentException(Exception e){
-    return "<h1>"+e.getMessage()+"</h1>";
+  public ResponseDto<String> handleHttpStatusception(Exception e){
+    return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
   }
 }
