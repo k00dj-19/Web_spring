6eeb473d50e9d99c87939df2f98292a9c19ff851
김동진 <rlaehdwls310@khu.ac.kr>
2021-12-20 10:35:00 +0000

    글 상세보기

--- src/main/java/project/controller/BoardController.java
+++ src/main/java/project/controller/BoardController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import project.config.auth.PrincipalDetail;
@@ -22,6 +22,12 @@ public class BoardController {
   public String index(Model model) {
     model.addAttribute("boards", boardService.글목록());
     return "index";
+  }
+  
+  @GetMapping("/board/{id}")
+  public String findById(@PathVariable int id, Model model){
+    model.addAttribute("board", boardService.글상세보기(id));
+    return "board/detail";
   }
   
   // USER 권한이 필요
