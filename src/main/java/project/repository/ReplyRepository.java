d40fcb310bbcfcdca794532d52fb3ed84ce2bf6f
김동진 <rlaehdwls310@khu.ac.kr>
2022-01-18 13:25:45 +0000

    댓글 작성시 네이티브 쿼리 사용해보기

--- src/main/java/project/repository/ReplyRepository.java
+++ src/main/java/project/repository/ReplyRepository.java
@@ -1,8 +1,14 @@
 package project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
 
+import project.dto.ReplySaveRequestDto;
 import project.model.Reply;
 
 public interface ReplyRepository extends JpaRepository<Reply, Integer>{
+  @Modifying
+  @Query(value="INSERT INTO reply(userId, boardId, content, createDate) VALUES(?1,?2,?3,now())", nativeQuery = true)
+  int mSave(int userId, int boardId, String content); // 업데이트 된 행의 개수를 리턴해줌.
 }
